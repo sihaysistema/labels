d021dd32b458ea3fd5947067470a6267b5a5aaa0
monroy95 <m.monroyc22@gmail.com>
2019-04-08 15:46:34 -0600

    Agregnado script Alain

---
+++ labels/labels/doctype/pdf_label_barcode_generator/pdf_label_barcode_generator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Si Hay Sistema and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PDF Label Barcode Generator', {
+	refresh: function(frm) {
+
+	}
+});
