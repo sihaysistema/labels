1290d3c8f9fec6c2e968af8a9a4c0fedefa75a55
monroy95 <m.monroyc22@gmail.com>
2019-04-11 15:20:37 -0600

    Cleaning dict, adding requirements python

--- labels/api_labels.py
+++ labels/api_labels.py
@@ -18,11 +18,13 @@ def child_table_to_csv(dict_data):
 
     dict_data_csv = json.loads(dict_data)
 
-    csv_file = 'salida.csv'
-    csv_columns = []
+    # frappe.msgprint(_(str(dict_data)))
 
-    for column in dict_data_csv[0]:
-        csv_columns.append(str(column))
+    csv_file = 'salida.csv'
+    csv_columns = ['item_code', 'item_name', 'quantity']
+
+    # for column in dict_data_csv[0]:
+    #     csv_columns.append(str(column))
 
     try:
         with open(csv_file, 'w') as csvfile:
@@ -30,6 +32,21 @@ def child_table_to_csv(dict_data):
             writer.writeheader()
 
             for data in dict_data_csv:
+                del data['modified_by']
+                del data['parent']
+                del data['creation']
+                del data['modified']
+                del data['min_order_qty']
+                del data['idx']
+                del data['parenttype']
+                del data['warehouse']
+                del data['doctype']
+                del data['owner']
+                del data['docstatus']
+                del data['requested_qty']
+                del data['actual_qty']
+                del data['parentfield']
+                del data['name']
                 writer.writerow(data)
 
     except IOError:
@@ -38,7 +55,7 @@ def child_table_to_csv(dict_data):
         frappe.msgprint(_('OK'))
 
     ## required columns
-    # Item Code : ok
+    # Item Code : ok -> jalar name, en ves de description is name
     # Description : add custom field
     # Stock UOM : ok
     # Required Qty : custom field?
@@ -46,3 +63,5 @@ def child_table_to_csv(dict_data):
     # Quantity Requested for Purchase : custom field ?
     # Ordered Qty : ok
     # Actual Qty : ok
+
+# Basado en la fecha de posting del DT
