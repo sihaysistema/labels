08d55abb4b0834177ca129a5d35d2b1338385f9c
Tropicalrambler <alainber@gmail.com>
2020-07-12 17:53:48 +0000

    #6 doctypes for label formatting

---
+++ labels/labels/doctype/label_component/label_component.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Si Hay Sistema and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Label Component', {
+	// refresh: function(frm) {
+
+	// }
+});
